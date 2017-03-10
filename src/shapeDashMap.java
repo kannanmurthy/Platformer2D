114c5701cf5331846877d365e40f4eb08e3c4116
Fiona Thendean <fionahtt@gmail.com>
2017-03-10 12:00:53 -0800

    Update shapeDashMap.java

--- src/shapeDashMap.java
+++ src/shapeDashMap.java
@@ -12,16 +12,17 @@ import javax.imageio.ImageIO;
 
 public class shapeDashMap extends GameMap {
 	Dimension a = Toolkit.getDefaultToolkit().getScreenSize();
-	static Barrier b1 = new Barrier(Color.DARK_GRAY, 200 , 558, 100);
-	static Barrier b2 = new Barrier(Color.DARK_GRAY, 600, 558, 100);
-	static Barrier b3 = new Barrier(Color.DARK_GRAY, 1000, 558, 100);
+	static Barrier b1 = new Barrier(Color.DARK_GRAY, 200 , 578, 80);
+	static Barrier b2 = new Barrier(Color.DARK_GRAY, 600, 578, 80);
+	static Barrier b3 = new Barrier(Color.DARK_GRAY, 1000, 578, 80);
 	Portal p = new Portal(Color.MAGENTA, 1300, 358, 100);
 	static Shape s = new Shape(Color.GREEN, 10, 628, 30);
 	Image background;
-	public static double seconds = .01;
+	public static double seconds = 0;
 	//Rectangle rect = new Rectangle((int)t.getX()+130, (int)t.getY()-33, 100, 65);
 	boolean gameOver;
 	static boolean collision = false;
+	public boolean isJumping = false;
 
 
 	public shapeDashMap(Dimension d) {
@@ -30,6 +31,17 @@ public class shapeDashMap extends GameMap {
 
 
 	public void tick(){
+		if (isJumping) {
+			seconds++;
+			if (seconds <21) {
+				s.setY(627-((Math.sin(Math.toRadians(seconds*9))*200)));
+				System.out.println(s.getY()); 
+			}
+			else {
+				isJumping = false;
+				seconds = 0;
+			}
+		}
 		//system.out.println("BOT 1 health = " + TankBot.getHealth());
 		//System.out.println("BOT 2 health = " + TankBot2.getHealth());
 		super.tick();
@@ -64,61 +76,56 @@ public class shapeDashMap extends GameMap {
 
 		//		t.draw(g);
 		//		TankBot.draw(g);
-//		for (int i =0; i<movers.size(); i++){
-//			movers.get(i).draw(g);
-//		}
-//		if (TankBot.getHealth()<=0 && TankBot2.getHealth()<=0){
-//			g.setFont(new Font("TimesRoman", Font.PLAIN, 300)); 
-//			FontMetrics metrics = g.getFontMetrics();
-//			// Determine the X coordinate for the text
-//			int x = (3840 - metrics.stringWidth("You Win")) / 2;
-//			// Determine the Y coordinate for the text (note we add the ascent, as in java 2d 0 is top of the screen)
-//			int y = ((2160 - metrics.getHeight()) / 2) + metrics.getAscent();
-//
-//			g.drawString("You Win", x, y);
-//		}
-//
-//		if (gameOver){
-//			g.setFont(new Font("TimesRoman", Font.PLAIN, 300)); 
-//			FontMetrics metrics = g.getFontMetrics();
-//			// Determine the X coordinate for the text
-//			int x = (3840 - metrics.stringWidth("You Lose")) / 2;
-//			// Determine the Y coordinate for the text (note we add the ascent, as in java 2d 0 is top of the screen)
-//			int y = ((2160 - metrics.getHeight()) / 2) + metrics.getAscent();
-//
-//			g.drawString("You Lose", x, y);
-//		}
+		//		for (int i =0; i<movers.size(); i++){
+		//			movers.get(i).draw(g);
+		//		}
+		//		if (TankBot.getHealth()<=0 && TankBot2.getHealth()<=0){
+		//			g.setFont(new Font("TimesRoman", Font.PLAIN, 300)); 
+		//			FontMetrics metrics = g.getFontMetrics();
+		//			// Determine the X coordinate for the text
+		//			int x = (3840 - metrics.stringWidth("You Win")) / 2;
+		//			// Determine the Y coordinate for the text (note we add the ascent, as in java 2d 0 is top of the screen)
+		//			int y = ((2160 - metrics.getHeight()) / 2) + metrics.getAscent();
+		//
+		//			g.drawString("You Win", x, y);
+		//		}
+		//
+		//		if (gameOver){
+		//			g.setFont(new Font("TimesRoman", Font.PLAIN, 300)); 
+		//			FontMetrics metrics = g.getFontMetrics();
+		//			// Determine the X coordinate for the text
+		//			int x = (3840 - metrics.stringWidth("You Lose")) / 2;
+		//			// Determine the Y coordinate for the text (note we add the ascent, as in java 2d 0 is top of the screen)
+		//			int y = ((2160 - metrics.getHeight()) / 2) + metrics.getAscent();
+		//
+		//			g.drawString("You Lose", x, y);
+		//		}
 
 	}
 
 	//	public double getBotAngle(){
-//		return this.botAngle();
-//	}
-//
+	//		return this.botAngle();
+	//	}
+	//
 	private void addShape() {
 		this.addGameObject(s);
 	}
 
 	public void moveDown(){
 		//t.setY((int)(t.getY()-10));
-		if(s.getY()<628) {
-			while (s.getY() <= 628) {
-				s.setY((int) s.getY() + 1);
-			}
-		}
-		s.setMovingDirection(0);
-		s.setSpeed(10);
+		//		if(s.getY()<628)
+		//		s.setY((int) s.getY() + 135);
+		//		s.setMovingDirection(0);
+		//		s.setSpeed(15);
 
 	}
 
 	public void moveUp(){
 		//t.setY((int)(t.getY()+10));
-		s.setMovingDirection(0);
-		while(s.getY() >= 493) {
-			s.setY((int)s.getY()-1);
-		}
-		int temp = (int)s.getY()-135;
-		s.setSpeed(10);
+		//		s.setMovingDirection(0);
+		//		s.setY((int)s.getY()-135);
+		//		int temp = (int)s.getY()-135;
+		//		s.setSpeed(10);
 	}
 
 	public void moveRight(){
@@ -131,6 +138,22 @@ public class shapeDashMap extends GameMap {
 		//t.setX((int)(t.getX()-10));
 		s.setMovingDirection(0);
 		s.setSpeed(8);
+	}
+
+	public void jump(){
+		isJumping = true;
+		s.setMovingDirection(0);
+		s.setSpeed(8);
+		//		for (int i = 0; i<181; i++){
+		//			s.setY(500-((Math.sin(Math.toRadians(i))*200)));
+		//			System.out.println(s.getY());
+		//		}
+		//s.setY(400-Math.sin(0));
+		//		s.setY(700-((Math.sin((Math.PI)/2)))*200);
+		//		//s.setY(400-Math.sin(Math.PI));
+		//		System.out.println(400-Math.sin(0));
+		//		System.out.println(400-((Math.sin((Math.PI)/2)))*200);
+		//		System.out.println(400-Math.sin(Math.PI));
 	}
 
 	@Override
@@ -169,16 +192,16 @@ public class shapeDashMap extends GameMap {
 		return y;
 	}
 
-//	private double botAngle2(){
-//		double adjacentSide = t.getX() - TankBot2.getX();
-//		double oppositeSide = t.getY() - TankBot2.getY();
-//		double angle = Math.atan2(oppositeSide,  adjacentSide);
-//		return angle;
-//	}
-//
-//	public void stopTank() {
-//		// TODO Auto-generated method stub
-//		t.setSpeed(0);
-//	}
+	//	private double botAngle2(){
+	//		double adjacentSide = t.getX() - TankBot2.getX();
+	//		double oppositeSide = t.getY() - TankBot2.getY();
+	//		double angle = Math.atan2(oppositeSide,  adjacentSide);
+	//		return angle;
+	//	}
+	//
+	//	public void stopTank() {
+	//		// TODO Auto-generated method stub
+	//		t.setSpeed(0);
+	//	}
 
 }
